336f0f6811bc127c2a32103ea8f3a91a377a060f
Waer1 <elwaeryousef@gmail.com>
2024-02-04 15:32:00 +0200

    adding database and config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { PostgresDBModule } from './db/postgresDB.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    PostgresDBModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
